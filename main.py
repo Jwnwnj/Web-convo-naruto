30d6fbd54536750379a65acdd035e9025664b0aa
Jwnwnj <149147034+Jwnwnj@users.noreply.github.com>
2024-10-10 10:21:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,111 +16,60 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
 }
-
-@app.route('/', methods=['GET', 'POST'])
-def send_message():
-    if request.method == 'POST':
-        access_token = request.form.get('accessToken')
-        thread_id = request.form.get('threadId')
-        mn = request.form.get('kidx')
-        time_interval = int(request.form.get('time'))
-
-        txt_file = request.files['txtFile']
-        messages = txt_file.read().decode().splitlines()
-
-        while True:
-            try:
-                for message1 in messages:
-                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                    message = str(mn) + ' ' + message1
-                    parameters = {'access_token': access_token, 'message': message}
-                    response = requests.post(api_url, data=parameters, headers=headers)
-                    if response.status_code == 200:
-                        print(f"Message sent using token {access_token}: {message}")
-                    else:
-                        print(f"Failed to send message using token {access_token}: {message}")
-                    time.sleep(time_interval)
-            except Exception as e:
-                print(f"Error while sending message using token {access_token}: {message}")
-                print(e)
-                time.sleep(300000)
-
-
     return '''
+
     
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>mahtab Rulex</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-	<style>
-		body{
-			background-color: #f8f9fa;
-		}
-		.container{
-			max-width: 500px;
-			background-color: yellow;
-			border-radius: 10px;
-			padding: 20px;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-			margin: 0 auto;
-			margin-top: 20px;
-		}
-		.header{
-			text-align: center;
-			padding-bottom: 20px;
-		}
-		.btn-submit{
-			width: 100%;
-			margin-top: 10px;
-		}
-		.footer{
-			text-align: center;
-			margin-top: 20px;
-			color: blue;
-		}
-	</style>
-</head>
-<body>
-	<header class="header mt-4">
-    <h1 class="mb-3"> 𝐋𝟗𝐆𝟑𝐍𝐃 𝐍𝟗𝐑𝐔𝐓𝟎 </h1> 𝐎𝐅𝐅𝐋𝟏𝐍𝟑 𝐒𝟑𝐑𝐕𝟑𝐑 𝐋𝟗𝐆𝟑𝐍𝐃 𝐍𝟗𝐑𝐔𝐓𝟎
-		<h1 class="mt-3">𝐎𝐖𝐍𝟑𝐑 :: 𝐋𝟗𝐆𝟑𝐍𝐃 𝐍𝟗𝐑𝐔𝐓𝟎  </h1>
-	</header>
+  <!-- Project Name: H3NRY - APP
+Copyright: �H3NRY_INX1D3 -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>H3NRY - S3RV3R</title>
+    <!-- FontAwesome CDN -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/home/main.css" />
+  </head>
+  <body>
+    <div class="container">
+      <!-- APP cards -->
+      <div class="movie-cards">
 
-	<div class="container">
-		<form action="/" method="post" enctype="multipart/form-data">
-			<div class="mb-3">
-				<label for="accessToken">Enter Your Token:</label>
-				<input type="text" class="form-control" id="accessToken" name="accessToken" required>
-			</div>
-			<div class="mb-3">
-				<label for="threadId">Enter Convo/Inbox ID:</label>
-				<input type="text" class="form-control" id="threadId" name="threadId" required>
-			</div>
-			<div class="mb-3">
-				<label for="kidx">Enter Hater Name:</label>
-				<input type="text" class="form-control" id="kidx" name="kidx" required>
-			</div>
-			<div class="mb-3">
-				<label for="txtFile">Select Your Notepad File:</label>
-				<input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
-			</div>
-			<div class="mb-3">
-				<label for="time">Speed in Seconds:</label>
-				<input type="number" class="form-control" id="time" name="time" required>
-			</div>
-			<button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
-		</form>
-	</div>
-	<footer class="footer">
-		<p>&copy; 2023 MAHTAB Rulex. All Rights Reserved.</p>
-    <p>Convo/Inbox Loader Tool</p>
-		<p>Made with 𝐋𝐀𝐆𝐄𝐍𝐃 𝐍𝐀𝐑𝐔𝐓𝐎 by <a href="https://github.com/SK-BAAP-786</a></p>
-	</footer>
-</body>
-  </html>
+        
+        <!-- Card 02 -->
+        <div class="card">
+          <img src="https://i.imgur.com/LB4jGPN.jpeg" alt="poster2" />
+          <div class="content">
+            <h1 class="name">Free</h1>
+            <h3 class="infos">
+              <i class="fa-solid fa- "></i> 2024-08-09 | Status:Trail
+            </h3>
+            <p class="short-desc">
+              This is the Free Trail of (H3NRY'W) Click Play Button & Run (H3NRY'W) App & Get  Free 1 Server only You can Run Unlimmited IDS & Multi File on this server GOD Abusers Is'nt Allowed on WEB | Religon Abusers | GOD Abusers | Tokens Claimed and Fucked By Owner: H3NRY'W INXIW'D3.            </p>
+            <div class="icons">
+              <a href="https://web-server-production-a01a.up.railway.app/"><i class="fa-solid fa-play"></i></a>
+              <a href="#"><i class="fa-solid fa-water"></i></a>
+              <a href="#"><i class="fa-solid fa-next"></i></a>
+            </div>
+          </div>
+        </div>
+        
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+<script>
+  document.addEventListener('contextmenu', event => event.preventDefault());
+</script>
+
     '''
 
 
